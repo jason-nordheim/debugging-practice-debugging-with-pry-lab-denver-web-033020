e275105021d3501ff2b75c33ab6241c890f9449d
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-14 20:10:59 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -4,8 +4,10 @@ def snake_it_up(string)
   if string[0] == "s"
     result = ((10 * "s") + string)
     binding.pry
-    return result 
+    return result
   else
     return string
   end
 end
+
+snake_it_up("sample")
